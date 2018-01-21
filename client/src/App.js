cdaab70ee021991bfd4a4d546f0da0cfda1ed7bf
Srivatsava Chimakurthi <csrivatsava@csri.attlocal.net>
2018-01-21 11:14:55 -0800

    added code to use passport, still needs to test the code

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import Books from "./pages/Books";
 // import Detail from "./pages/Detail";
+import Auth from './pages/Auth'
+import PrivateRoute from './utils/PrivateRoute'
+
 import Home from "./pages/Home";
 import Flights from "./pages/Flights";
 import Cars from "./pages/Cars";
 import Hotels from "./pages/Hotels";
 import NoMatch from "./pages/NoMatch";
+// import User from "./pages/User";
+// import Checkout from "./pages/Checkout";
 
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
@@ -20,6 +25,11 @@ const App = () =>
         <Route exact path="/flights" component={Flights} />
         <Route exact path="/cars" component={Cars} />
         <Route exact path="/hotels" component={Hotels} />
+
+        <Route exact path='/login' component={Auth} />
+
+        {/* <PrivateRoute exact path='/user' component={User} /> */}
+        {/* <PrivateRoute exact path='/checkout' component={Checkout} /> */}
         {/* <Route exact path="/" component={Users} /> */}
         {/* <Route exact path="/" component={Checkout} /> */}
         
@@ -27,6 +37,7 @@ const App = () =>
         {/* <Route exact path="/" component={Books} /> */}
         {/* <Route exact path="/books" component={Books} /> */}
         {/* <Route exact path="/books/:id" component={Detail} /> */}
+
         <Route component={NoMatch} />
       </Switch>
       <Footer />
--- client/src/pages/Flights/Flights.js
+++ client/src/pages/Flights/Flights.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
-import { Col, Row, Container } from "../../components/Grid";
-import { Input, TextArea, FormBtn } from "../../components/Form";
-import API from "../../utils/API";
+// import React, { Component } from "react";
+// import { Col, Row, Container } from "../../components/Grid";
+// import { Input, TextArea, FormBtn } from "../../components/Form";
+// import API from "../../utils/API";
 
-import Nav from "../../components/Nav";
-import Hero from "../../components/Hero";
-import Contact from "../../components/Contact";
-import Footer from "../../components/Footer";
+// import Nav from "../../components/Nav";
+// import Hero from "../../components/Hero";
+// import Contact from "../../components/Contact";
+// import Footer from "../../components/Footer";
