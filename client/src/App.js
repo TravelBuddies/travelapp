cfae68aed69a40d7e0577e519d0b23a012c9f412
Carmen Stockberger <carmen.stockberger@gmail.com>
2018-01-21 00:37:22 -0800

    added logic to render different form pages for flight, hotel, and car rentals (WIP)

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import Books from "./pages/Books";
-// import Detail from "./pages/Detail";
+
 import Home from "./pages/Home";
 import Flights from "./pages/Flights";
 import Cars from "./pages/Cars";
 import Hotels from "./pages/Hotels";
+import Packages from "./pages/Packages";
 import NoMatch from "./pages/NoMatch";
-
+import Contact from "./components/Contact";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 
@@ -20,15 +20,13 @@ const App = () =>
         <Route exact path="/flights" component={Flights} />
         <Route exact path="/cars" component={Cars} />
         <Route exact path="/hotels" component={Hotels} />
+        <Route exact path="/packages" component={Packages} />
         {/* <Route exact path="/" component={Users} /> */}
         {/* <Route exact path="/" component={Checkout} /> */}
-        
-        
-        {/* <Route exact path="/" component={Books} /> */}
-        {/* <Route exact path="/books" component={Books} /> */}
-        {/* <Route exact path="/books/:id" component={Detail} /> */}
+
         <Route component={NoMatch} />
       </Switch>
+      <Contact />
       <Footer />
     </div>
   </Router>;
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,14 +3,19 @@ import { Col, Row, Container } from "../../components/Grid";
 import { Input, TextArea, FormBtn } from "../../components/Form";
 import API from "../../utils/API";
 
-import Nav from "../../components/Nav";
+// import Nav from "../../components/Nav";
+import Navpills from "../../components/Nav"
 import Hero from "../../components/Hero";
+import Flights from "../Flights";
+import Cars from "../Cars";
+import Hotels from "../Hotels";
+import Packages from "../Packages";
 import Contact from "../../components/Contact";
 import Footer from "../../components/Footer";
 
 class Home extends Component {
   state = {
-    Home: [],
+    currentPage: "Home",
   // update here if any form details that are needed to be empty.
   };
 
@@ -21,104 +26,99 @@ class Home extends Component {
     // this.loadPackageDeals();
   }
 
-  loadFlights = () => {
-    API.getFlights()
-      .then(res =>
-        this.setState({ flights: res.data, title: "", author: "", synopsis: "" }) // make sure to empty the strings on the screen.
-      )
-      .catch(err => console.log(err));
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
   };
 
+  // handleInputChange = event => {
+  //   const { name, value } = event.target;
+  //   this.setState({
+  //     [name]: value
+  //   });
+  // };
 
-//   deleteBook = id => {
-//     API.deleteBook(id)
-//       .then(res => this.loadBooks())
-//       .catch(err => console.log(err));
-//   };
-
-  handleInputChange = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
-
-  handleFormSubmit = event => {
-    event.preventDefault();
-    if (this.state.title && this.state.author) {
-      API.saveFlight({
-        title: this.state.title,
-        author: this.state.author,
-        synopsis: this.state.synopsis
-      })
-        .then(res => this.loadFlights())
-        .catch(err => console.log(err));
-    }
-  };
+  // handleFormSubmit = event => {
+  //   event.preventDefault();
+  //   if (this.state.title && this.state.author) {
+  //     API.saveFlight({
+  //       title: this.state.title,
+  //       author: this.state.author,
+  //       synopsis: this.state.synopsis
+  //     })
+  //       .then(res => this.loadFlights())
+  //       .catch(err => console.log(err));
+  //   }
+  // };
 
   render() {
     return (
-      <div>
-        <Nav></Nav>
+      <div>        
         <Hero></Hero>
+          
+<div class="featured-image-block-grid">
+  <div class="featured-image-block-grid-header small-10 medium-8 large-7 columns text-center">
+    <h2 id="destinationHeader">Top Destinations</h2>
+    <p>Start planning your travels here with the latest deals for popular destinations.</p>
+  </div>
+  <div class="row large-up-4 small-up-2">
+    <div class="featured-image-block column">
+     <a href="#">
+        <img src="https://unsplash.it/600/440?image=899" />
+        <p class="text-center featured-image-block-title">New York</p>
+      </a>
+    </div>
+    
+    <div class="featured-image-block column">
+     <a href="#">
+        <img src="https://unsplash.it/600/440?image=693" />
+        <p class="text-center featured-image-block-title">San Francisco</p>
+      </a>
+    </div>
+    
+    <div class="featured-image-block column">
+     <a href="#">
+        <img src="https://unsplash.it/600/440?image=452" />
+        <p class="text-center featured-image-block-title">Las Vegas</p>
+      </a>
+    </div>
+    
+    <div class="featured-image-block column">
+     <a href="#">
+        <img src="https://unsplash.it/600/440?image=700" />
+        <p class="text-center featured-image-block-title">Sacramento</p>
+      </a>
+    </div>
+    
+    <div class="featured-image-block column">
+     <a href="#">
+        <img src="https://unsplash.it/600/440?image=667" />
+        <p class="text-center featured-image-block-title">Los Angeles</p>
+      </a>
+    </div>
+  
+    <div class="featured-image-block column">
+     <a href="#">
+        <img src="https://unsplash.it/600/440?image=249" />
+        <p class="text-center featured-image-block-title">Chicago</p>
+      </a>
+    </div>
+    
+    <div class="featured-image-block column">
+     <a href="#">
+        <img src="https://unsplash.it/600/440?image=382" />
+        <p class="text-center featured-image-block-title">Boston</p>
+      </a>
+    </div>
+
+    <div class="featured-image-block column">
+     <a href="#">
+        <img src="https://unsplash.it/600/440?image=1075" />
+        <p class="text-center featured-image-block-title">Seattle</p>
+      </a>
+    </div>
+  </div>
+</div>
         <Contact></Contact>
-        <Footer></Footer>
-        <Container fluid>
-          <Row>
-            <Col size="md-6">
-              {/* <Jumbotron> */}
-                <h1>What Books Should I Read?</h1>
-              {/* </Jumbotron> */}
-              {/* <form>
-                <Input
-                  value={this.state.title}
-                  onChange={this.handleInputChange}
-                  name="title"
-                  placeholder="Title (required)"
-                />
-                <Input
-                  value={this.state.author}
-                  onChange={this.handleInputChange}
-                  name="author"
-                  placeholder="Author (required)"
-                />
-                <TextArea
-                  value={this.state.synopsis}
-                  onChange={this.handleInputChange}
-                  name="synopsis"
-                  placeholder="Synopsis (Optional)"
-                />
-                <FormBtn
-                  disabled={!(this.state.author && this.state.title)}
-                  onClick={this.handleFormSubmit}
-                >
-                  Submit Book
-                </FormBtn>
-              </form> */}
-            </Col>
-            {/* <Col size="md-6 sm-12">
-              <Jumbotron>
-                <h1>Books On My List</h1>
-              </Jumbotron>
-              {this.state.books.length ? (
-                <List>
-                  {this.state.books.map(book => (
-                    <ListItem key={book._id}>
-                      <Link to={"/books/" + book._id}>
-                        <strong>
-                          {book.title} by {book.author}
-                        </strong>
-                      </Link>
-                      <DeleteBtn onClick={() => this.deleteBook(book._id)} />
-                    </ListItem>
-                  ))}
-                </List>
-              ) : (
-                <h3>No Results to Display</h3>
-              )}
-            </Col> */}
-          </Row>
-        </Container>
       </div>  
     );
   }
