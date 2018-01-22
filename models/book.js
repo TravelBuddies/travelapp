bd12c64bcf18293c1e8421fffb757f0de7d77974
Jachaplin <jakealanchaplin@yahoo.com>
2018-01-21 17:34:03 -0800

    database setup

--- models/book.js
+++ models/book.js
@@ -1,13 +1,29 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const bookSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
+// const bookSchema = new Schema({
+//   title: { type: String, required: true },
+//   author: { type: String, required: true },
+//   synopsis: String,
+//   date: { type: Date, default: Date.now }
+// });
+
+// const Book = mongoose.model("Book", bookSchema);
+
+// module.exports = Book;
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  password: { type: String, required: true },
+  address: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  payment method: String,
+  flight: String,
+  hotel: String,
+  car: String,
   date: { type: Date, default: Date.now }
 });
 
-const Book = mongoose.model("Book", bookSchema);
+const User = mongoose.model("User", userSchema);
 
-module.exports = Book;
+module.exports = User;
