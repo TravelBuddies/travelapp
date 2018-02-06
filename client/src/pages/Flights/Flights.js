b8e4179fc098a10fbd7b8dbbfa1b6c0d220cb2da
Mark Joshua Bihag <markjoshuabihag@gmail.com>
2018-02-05 20:38:54 -0800

    fixed booking heros

--- client/src/pages/Flights/Flights.js
+++ client/src/pages/Flights/Flights.js
@@ -238,7 +238,7 @@ class Flights extends Component {
                                         </div>
 
 
-                                        
+                                         
                                     )}
                             </div>
                         </section>
